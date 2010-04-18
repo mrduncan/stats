2e96cb5b469d2f1ba7ed7ec0f491d93c93403928
Matt Duncan <mrduncan@gmail.com>
2010-04-18 11:46:31 -0400

    TimingStat now handles being empty

--- lib/stats/timing_stat.rb
+++ lib/stats/timing_stat.rb
@@ -2,17 +2,20 @@ module Stats
   class TimingStat < Array
     # Returns the average value.
     def average
+      return nil if length == 0
       sum / length.to_f
     end
 
     # Returns the population variance of the values.
     def population_variance
+      return nil if length == 0
       avg = average
       1 / length.to_f * inject(0) { |acc, i| acc + (i - avg) ** 2 }
     end
 
     # Returns the standard deviation of the values.
     def standard_deviation
+      return nil if length == 0
       Math.sqrt(population_variance)
     end
 
--- test/unit/test_timing_stat.rb
+++ test/unit/test_timing_stat.rb
@@ -7,11 +7,23 @@ class TestStats < Test::Unit::TestCase
     assert_equal 2.0, Stats::TimingStat.new([1, 2, 2, 3]).average
   end
 
+  def test_should_have_average_of_nil_with_no_data
+    assert_nil Stats::TimingStat.new([]).average
+  end
+
   def test_should_get_population_variance
     assert_equal 0.25, Stats::TimingStat.new([1, 1, 2, 2]).population_variance
   end
 
+  def test_should_have_population_variance_of_nil_with_no_data
+    assert_nil Stats::TimingStat.new([]).population_variance
+  end
+
   def test_should_get_standard_deviation
     assert_equal 0.5, Stats::TimingStat.new([1, 1, 2, 2]).standard_deviation
+  end
+
+  def test_should_have_standard_deviation_of_nil_with_no_data
+    assert_nil Stats::TimingStat.new([]).standard_deviation
   end
 end
